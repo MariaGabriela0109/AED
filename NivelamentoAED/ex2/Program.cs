32f3df985baabd02509247b2a0a7388c6fc519e2
Maria Gabriela Delfim <mari.gabriela0109@gmail.com>
2026-02-16 11:05:13 -0300

    Adicionando restante dos exercícios

---
+++ NivelamentoAED/ex2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2
+{
+    internal class Program
+    {
+        static void LerVetor(int[] vetor, string nome)
+        {
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.Write(nome + "[" + i + "]: ");
+                vetor[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor de N: ");
+            int N = int.Parse(Console.ReadLine());
+
+            int[] x = new int[N];
+            int[] y = new int[N];
+
+            LerVetor(x, "X");
+            LerVetor(y, "Y");
+
+            Console.WriteLine("Soma:");
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write(x[i] + y[i] + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Produto:");
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write(x[i] * y[i] + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Diferenca (X - Y):");
+            for (int i = 0; i < N; i++)
+            {
+                bool existe = false;
+
+                for (int j = 0; j < N; j++)
+                {
+                    if (x[i] == y[j])
+                    {
+                        existe = true;
+                    }
+                }
+
+                if (!existe)
+                {
+                    Console.Write(x[i] + " ");
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Intersecao:");
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (x[i] == y[j])
+                    {
+                        Console.Write(x[i] + " ");
+                    }
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Uniao:");
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write(x[i] + " ");
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                bool existe = false;
+
+                for (int j = 0; j < N; j++)
+                {
+                    if (y[i] == x[j])
+                    {
+                        existe = true;
+                    }
+                }
+
+                if (!existe)
+                {
+                    Console.Write(y[i] + " ");
+                }
+            }
+        }
+    }
+}
---
+++ NivelamentoAED/ex4/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex4
+{
+    internal class Program
+    {
+        static int LerVotos(int[] votos)
+        {
+            int nulos = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write("Voto do aluno " + (i + 1) + ": ");
+                int numero = int.Parse(Console.ReadLine());
+
+                if (numero >= 0 && numero < votos.Length)
+                {
+                    votos[numero]++;
+                }
+                else
+                {
+                    nulos++;
+                }
+            }
+
+            return nulos;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Quantidade de candidatos: ");
+            int qtd = int.Parse(Console.ReadLine());
+
+            string[] nomes = new string[qtd];
+            int[] votos = new int[qtd];
+
+            for (int i = 0; i < qtd; i++)
+            {
+                Console.Write("Nome do candidato " + i + ": ");
+                nomes[i] = Console.ReadLine();
+            }
+
+            int nulos = LerVotos(votos);
+
+            int maior = votos[0];
+            int menor = votos[0];
+            int posMaior = 0;
+            int posMenor = 0;
+
+            for (int i = 1; i < qtd; i++)
+            {
+                if (votos[i] > maior)
+                {
+                    maior = votos[i];
+                    posMaior = i;
+                }
+
+                if (votos[i] < menor)
+                {
+                    menor = votos[i];
+                    posMenor = i;
+                }
+            }
+
+            Console.WriteLine("Mais votado: " + nomes[posMaior] + " - " + maior + " votos");
+            Console.WriteLine("Menos votado: numero " + posMenor + " - " + menor + " votos");
+            Console.WriteLine("Votos nulos: " + nulos);
+        }
+    }
+}
