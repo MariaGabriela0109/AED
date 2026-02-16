26f21fa836a5d5659465e0f82ec18fd769e9376b
Maria Gabriela Delfim <mari.gabriela0109@gmail.com>
2026-02-16 10:23:34 -0300

    Estrutura inicial: Solução com ex1 e ex3

---
+++ NivelamentoAED/ex1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nivelamento01AED
+{
+    internal class ex1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            int aux = 1;
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+
+                    Console.Write((aux) + " ");
+                    aux++;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ NivelamentoAED/ex3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int aux = 0;
+            Random r = new Random();
+            int[] vet = new int[1000];
+
+            for (int i = 0; i < vet.Length; i++)
+            {
+                vet[i] = r.Next(1, 1001);
+            }
+
+            for (int i = 0; i < vet.Length; i++)
+            {
+                Console.Write(vet[i] + ", ");
+            }
+
+            for (int i = 1; i < vet.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (vet[i] < vet[j])
+                    {
+                        aux = vet[i];
+                        vet[i] = vet[j];
+                        vet[j] = aux;
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------------------------");
+
+            for (int i = 0; i < vet.Length; i++)
+            {
+                Console.Write(vet[i] + ", ");
+            }
+        }
+    }
+}
