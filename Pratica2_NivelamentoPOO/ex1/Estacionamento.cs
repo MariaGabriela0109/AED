2090bbace951080528ae3f31a795d7c7ccf2e826
Maria Gabriela Delfim <mari.gabriela0109@gmail.com>
2026-02-25 22:23:34 -0300

    inicio exercício estacionamento

---
+++ Pratica2_NivelamentoPOO/ex1/Estacionamento.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex1
+{
+    internal class Estacionamento
+    {
+        private string nome;
+        private int numVagasLivres;
+        private string[] vagas;
+
+        public int NumVagasLivres
+        {
+            get { return numVagasLivres; }
+        }
+        public Estacionamento(string nome, int numTotalVagas)
+        {
+            this.nome = nome;
+            this.numVagasLivres = numTotalVagas;
+            vagas = new string[numTotalVagas];
+        }
+        public int Estacionar(string placa)
+        {
+            for(int i = 0; i < vagas.Length; i++)
+            {
+                if (vagas[i] == null)
+                {
+                    vagas[i] = placa;
+                    numVagasLivres--;
+                    return i;
+
+                }
+            }
+            return -1;
+        }
+        public int BuscarNumVaga(string placa)
+        {
+            for (int i = 0; i < vagas.Length; i++)
+            {
+                if (vagas[i] == placa)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public void Retirar(string placa)
+        {
+            for (int i = 0; i < vagas.Length; i++)
+            {
+                if (vagas[i] == placa)
+                {
+                    vagas[i] = null;
+                    numVagasLivres++;
+                }
+           
+            }
+        }
+
+        public void ExibirOcupacao()
+        {
+            for (int i = 0; i < vagas.Length; i++)
+            {
+                if (vagas[i] != null)
+                {
+                    Console.WriteLine($"Vaga {i} ocupada por placa: {vagas[i]}");
+                }
+                Console.WriteLine($"A vaga {i} está vazia");
+            }
+        }
+        
+
+    }
+}
---
+++ Pratica2_NivelamentoPOO/ex1/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Estacionamento est = new Estacionamento("stop", 30);
+
+            if(est.Estacionar("1234") != -1)
+            {
+                Console.WriteLine($"O carro foi estacionado na vaga {est.Estacionar("1234")}");
+            }
+            else
+            {
+                Console.WriteLine("Estacionamento cheio");
+            }
+
+            if (est.Estacionar("8754") != -1)
+            {
+                Console.WriteLine($"O carro foi estacionado na vaga {est.Estacionar("8754")}");
+            }
+            else
+            {
+                Console.WriteLine("Estacionamento cheio");
+            }
+
+            if (est.Estacionar("9999") != -1)
+            {
+                Console.WriteLine($"O carro foi estacionado na vaga {est.Estacionar("9999")}");
+            }
+            else
+            {
+                Console.WriteLine("Estacionamento cheio");
+            }
+
+            if (est.Estacionar("8765") != -1)
+            {
+                Console.WriteLine($"O carro foi estacionado na vaga {est.Estacionar("8765")}");
+            }
+            else
+            {
+                Console.WriteLine("Estacionamento cheio");
+            }
+
+            if (est.Estacionar("9182") != -1)
+            {
+                Console.WriteLine($"O carro foi estacionado na vaga {est.Estacionar("9182")}");
+            }
+            else
+            {
+                Console.WriteLine("Estacionamento cheio");
+            }
+
+            est.ExibirOcupacao();
+
+            if (est.BuscarNumVaga("1234") != -1)
+            {
+                Console.WriteLine($"O carro foi estacionado na vaga {est.BuscarNumVaga("1234")}");
+            }
+            else
+            {
+                Console.WriteLine("Placa não encontrada.");
+            }
+
+            est.Retirar("9999");
+
+            est.ExibirOcupacao();
+
+            if (est.Estacionar("6655") != -1)
+            {
+                Console.WriteLine($"O carro foi estacionado na vaga {est.Estacionar("6655")}");
+            }
+            else
+            {
+                Console.WriteLine("Estacionamento cheio");
+            }
+
+            if (est.Estacionar("9000") != -1)
+            {
+                Console.WriteLine($"O carro foi estacionado na vaga {est.Estacionar("9000")}");
+            }
+            else
+            {
+                Console.WriteLine("Estacionamento cheio");
+            }
+
+            if (est.Estacionar("1111") != -1)
+            {
+                Console.WriteLine($"O carro foi estacionado na vaga {est.Estacionar("1111")}");
+            }
+            else
+            {
+                Console.WriteLine("Estacionamento cheio");
+            }
+
+            est.ExibirOcupacao();
+
+            int vagasLivres = est.NumVagasLivres;
+            Console.WriteLine($"Número de vagas livres: {vagasLivres}");
+
+        }
+    }
+}
