2de9e8a9823d2aa1931df7e07fa125b62f406875
Maria Gabriela Delfim <mari.gabriela0109@gmail.com>
2026-04-18 15:17:34 -0300

    Add ex

--- Lista6_List/menuDicionarioNoClass/Program.cs
+++ Lista6_List/menuDicionarioNoClass/Program.cs
@@ -8,8 +8,42 @@ namespace menuDicionarioNoClass
 {
     internal class Program
     {
+        public static Dictionary<string, string> Cadastrar(string palavra, string significado, Dictionary<string, string> dicionario)
+        {
+            if (dicionario.ContainsKey(palavra))
+            {
+            }
+            else
+            {
+                dicionario.Add(palavra, significado);
+            }
+            return dicionario;
+        }
+
+        public static Dictionary<string, string> Traduzir(string palavra, Dictionary<string, string> dicionario)
+        {
+            if (dicionario.ContainsKey(palavra))
+            {
+                Console.WriteLine(dicionario[palavra]);
+            }
+            else
+            {
+                Console.WriteLine("Palavra não encontrada");
+            }
+            return dicionario;
+        }
         static void Main(string[] args)
         {
+            Dictionary<string, string> dicionario = new Dictionary<string, string>();
+
+            Cadastrar("casa", "house", dicionario);
+            Traduzir("casa", dicionario);
+    
+                Console.WriteLine();
+    
+            Cadastrar("carro", "car", dicionario);
+            Traduzir("carro", dicionario);
+
         }
     }
 }
