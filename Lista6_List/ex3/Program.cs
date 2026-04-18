08bc4d79342652e644b92ca12ddc6a0c5492a4f0
Maria Gabriela Delfim <mari.gabriela0109@gmail.com>
2026-04-18 15:16:08 -0300

    Add ex

---
+++ Lista6_List/ex3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex3
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> decolagem = new Queue<string>();
+            int opcao = 0;
+
+            while (opcao != 6)
+            {
+                Console.WriteLine("Op:");
+                opcao = int.Parse(Console.ReadLine());
+
+                if (opcao == 1)
+                {
+                    Console.WriteLine(decolagem.Count);
+                }
+                else if (opcao == 2)
+                {
+                    if (decolagem.Count > 0)
+                    {
+                        string aviao = decolagem.Dequeue();
+                        Console.WriteLine(aviao);
+                    }
+                }
+                else if (opcao == 3)
+                {
+                    string novoAviao = Console.ReadLine();
+                    decolagem.Enqueue(novoAviao);
+                }
+                else if (opcao == 4)
+                {
+                    foreach (string aviao in decolagem)
+                    {
+                        Console.WriteLine(aviao);
+                    }
+                }
+                else if (opcao == 5)
+                {
+                    if (decolagem.Count > 0)
+                    {
+                        Console.WriteLine(decolagem.Peek());
+                    }
+                }
+            }
+        }
+    }
+}
+}
---
+++ Lista6_List/menuDicionario/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace menuDicionario
+{
+    public class Dicionario
+    {
+        public string palavra;
+        public string significado;
+
+        public Dicionario(string palavra, string significado)
+        {
+            this.palavra = palavra;
+            this.significado = significado;
+        }
+
+        Dictionary<string,string> dict = new Dictionary<string,string>();
+
+        public void Cadastrar()
+        {
+            if (dict.ContainsKey(palavra))
+            {
+
+            }
+            else
+            {
+                dict.Add(palavra, significado);
+            }
+              
+        }
+        public void Traduzir()
+        {
+            if (dict.ContainsKey(palavra))
+            {
+                Console.WriteLine(dict[palavra]);
+            }
+            else
+            {
+                Console.WriteLine("Palavra não encontrada");
+            }
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dicionario dict1 = new Dicionario("casa", "house");
+            Dicionario dict2 = new Dicionario("carro", "car");
+
+            dict1.Cadastrar();
+            dict1.Traduzir();
+
+            Console.WriteLine();
+
+            dict2.Cadastrar(); 
+            dict2.Traduzir();
+
+
+
+        }
+    }
+}
