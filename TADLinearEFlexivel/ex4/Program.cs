5f5881789b8c1c42a726c04c398014d53c5572d2
Maria Gabriela Delfim <mari.gabriela0109@gmail.com>
2026-05-17 00:53:10 -0300

    add ex.

--- TADLinearEFlexivel/ex4/Program.cs
+++ TADLinearEFlexivel/ex4/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,55 @@ using System.Threading.Tasks;
 
 namespace ex4
 {
+    class Arquivo
+    {
+        public string nome;
+        public int tam;
+
+        public Arquivo(string nome, int tam)
+        {
+            this.nome = nome;
+            this.tam = tam;
+        }
+    }
+
+    class Fila
+    {
+        public Arquivo[] array;
+        public int primeiro, ultimo;
+
+        public Fila(int tam)
+        {
+            array = new Arquivo[tam + 1];
+            primeiro = ultimo = 0;
+        }
+        public void Inserir(Arquivo arq)
+        {
+            if((ultimo + 1) % array.Length != primeiro)
+            {
+                array[ultimo] = arq;
+                ultimo = (ultimo + 1) % array.Length;
+            }
+            else
+            {
+                throw new Exception("fila cheia");
+            }
+        }
+
+        public Arquivo Remover()
+        {
+            if(ultimo == primeiro)
+            {
+                throw new Exception("Lista vazia");
+            }
+            else
+            {
+                Arquivo temp = array[primeiro];
+                primeiro = (primeiro + 1) % array.Length;
+                return temp;
+            }            
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
